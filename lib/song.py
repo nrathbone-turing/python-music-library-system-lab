276ee4042c2d45daea671a90fcc3dd0ece340453
Nick Rathbone <nrathbone@joinhandshake.com>
2025-08-09 11:53:25 -0600

    Create skeleton framework for Song class based on specs

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,33 @@
 class Song:
-    pass
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+    # name property
+
+
+    # artist property
+
+
+    # genre property
+
+
+    def add_song_to_count():
+        pass
+
+
+    def add_to_genres():
+        pass
+
+
+    def add_to_artists():
+        pass
+
+
+    def add_to_genre_count():
+        pass
+
+
+    def add_to_artists_count():
+        pass
